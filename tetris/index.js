947f56a1dcd1b83df570b0eb725dd19357b59a88
Billy Huang <billbox13@gmail.com>
2017-07-14 11:33:27 +1000

    Add matrix

--- tetris/index.js
+++ tetris/index.js
@@ -1,5 +1,23 @@
 const canvas = document.getElementById("tetris")
 const ctx = canvas.getContext('2d')
 
+ctx.scale(20,20)
+
 ctx.fillStyle = '#000'
 ctx.fillRect(0,0, canvas.width, canvas.height)
+
+const matrix = [
+    [0,0,0],
+    [1,1,1],
+    [0,1,0],
+]
+
+matrix.forEach((row,y) => {
+    row.forEach((value, x) => {
+        if (value !==0) {
+            ctx.fillStyle = 'red'
+            ctx.fillRect(x,y,1,1)
+        }
+    }) 
+})
+
