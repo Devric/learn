f415634bb91c978b78cb6cb9646583fbd6d722db
Billy Huang <billbox13@gmail.com>
2017-07-15 01:33:02 +1000

    allow matrix rotate

--- tetris/index.js
+++ tetris/index.js
@@ -115,6 +115,30 @@ function playerMove(dir) {
     }
 }
 
+function playerRotate(dir) {
+    rotate(player.matrix, dir)
+}
+
+function rotate(matrix,dir){
+    for (let y=0;y<matrix.length;y++){
+        for (let x=0; x<y; ++x) {
+            [
+                matrix[x][y],
+                matrix[y][x],
+            ] = [
+                matrix[y][x],
+                matrix[x][y],
+            ]
+        }
+    }
+
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse())
+    } else {
+        matrix.reverse()
+    }
+}
+
 document.addEventListener('keydown', e => {
     if(e.keyCode === 37) {
         playerMove(-1)
@@ -124,6 +148,14 @@ document.addEventListener('keydown', e => {
     }
     if(e.keyCode === 40) {
         playerDrop()
+    }
+    // w
+    if(e.keyCode === 81) {
+        playerRotate(-1)
+    }
+    // q
+    if(e.keyCode === 87) {
+        playerRotate(1)
     }
     
 })
