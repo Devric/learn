132cf3fe53475fee937692d6e105a69b3b773f84
Billy Huang <billbox13@gmail.com>
2017-07-20 00:37:43 +1000

    [PHASER TUT2] restart project

---
+++ phaser_tutorial_02/main.js
@@ -0,0 +1,16 @@
+var game = new Phaser.Game(640,360, Phaser.AUTO)
+
+var GameState = {
+    prelod: ()=>{
+
+    },
+    create: ()=>{
+
+    },
+    update: ()=>{
+
+    },
+}
+
+game.state.add('GameState', GameState)
+game.state.start('GameState')
