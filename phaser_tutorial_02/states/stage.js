e0d5b0c1e109d0ad77018c439b8af68dd77510e6
Billy Huang <billbox13@gmail.com>
2017-07-21 20:00:57 +1000

    [PHASER] Add wolf character

--- phaser_tutorial_02/states/stage.js
+++ phaser_tutorial_02/states/stage.js
@@ -3,7 +3,15 @@ GameGlobal.GameStates.stage = class StateStage {
         console.log('==>init:GameState:stage')
 
         this.assets = {
-            background : { type:'image',source:'assets/sky.png' },
+            background : { type : 'image'  , source : 'assets/sky.png' } ,
+            wolf       : { type : 'sprite' , source : "assets/baddie.png"  , width : 32 , height : 32 , fames :4 , margin : 0 , spacing : 0 }
+        }
+
+        this.StageAsset = {
+            background : null,
+            wolf       : null,
+            anim       : null,
+            loopText   : null,
         }
 
         this.groups = {}
@@ -19,6 +27,9 @@ GameGlobal.GameStates.stage = class StateStage {
         }
 
     }
+
+    // Phaser Game
+
     preload(){
         // Load Assets
         Util.assetLoader(this.game, this.assets)
@@ -26,6 +37,11 @@ GameGlobal.GameStates.stage = class StateStage {
     create(){
         this.background = this.game.add.sprite(0,0, 'background')
         this.game.add.text(16,16,'Stage Fight!',{fontSize:'32px',fill:'#000'})
+
+        this.wolf = this.game.add.sprite(100,100,'wolf')
+        this.wolfWalkLeft  = this.wolf.animations.add('walkLeft',[0,1])
+        this.wolfWalkRight = this.wolf.animations.add('walkRight',[2,3])
+        this.wolfWalkRight.play(5,true)
     }
     update(){
 
